cafb9e727d3bc57c2927117d96f6fda54aeb0845
Dzonyy <paszcza.jakub@gmail.com>
2021-01-24 05:35:39 +0100

    added user to types, create user registration

---
+++ components/user/Registration.tsx
@@ -0,0 +1,42 @@
+// components/AddPost.tsx
+
+import * as React from 'react'
+import { User } from '../../types'
+
+type Props = {
+  saveUser: (e: React.FormEvent, formData: User) => void
+}
+
+const Registration: React.FC<Props> = ({ savePost }) => {
+  const [formData, setFormData] = React.useState<ITrip>()
+
+  const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    })
+  }
+
+  return (
+    <form className='Form' onSubmit={(e) => saveUser(e, formData)}>
+      <div>
+        <div className='Form--field'>
+          <label htmlFor='name'>Title</label>
+          <input onChange={handleForm} type='text' id='title' />
+        </div>
+        <div className='Form--field'>
+          <label htmlFor='body'>Description</label>
+          <input onChange={handleForm} type='text' id='body' />
+        </div>
+      </div>
+      <button
+        className='Form__button'
+        disabled={formData === undefined ? true : false}
+      >
+        Zarejestruj się
+      </button>
+    </form>
+  )
+}
+
+export default Registration
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,11 @@ export interface ITrip {
   lat: string
   lng: string
 }
+
+export interface User {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+}
